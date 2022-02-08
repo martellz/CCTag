827c764e60218937814a918e65e06e7ba77a7584
martellz <54762388+martellz@users.noreply.github.com>
2022-02-08 16:53:07 +0800

    Update README.md

--- build_sh.sh
+++
@@ -1,2 +0,0 @@
-cd build_android
-NDK=/home/zyf/Android/Sdk/ndk/23.1.7779620/build/cmake/android.toolchain.cmake conan create .. zyf/testing -s os=Android -s os.api_level=21 -s arch=armv8 -s compiler=clang -s compiler.version=11 -s compiler.libcxx=libstdc++11 -b missing
